a505306fb6568e05ff1e92297e319e961f977bb8
Daniel <danielplimpias@gmail.com>
2023-04-06 22:03:42 -0400

    configurando el proyecto

--- app/src/main/java/com/example/runworkshop/MainActivity.kt
+++ app/src/main/java/com/example/runworkshop/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.runworkshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.runworkshop.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var retrofit: Retrofit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        retrofit = getRetrofit()
+        initUI()
+    }
+
+    private fun initUI() {
+
+    }
+
+    private fun getRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://polls.apiblueprint.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
+
