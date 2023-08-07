ee894d71bd7a76331e534ed3e44b4d346a736b09
Daniel <danielplimpias@gmail.com>
2023-08-07 15:20:25 -0400

    anhadimos notificaciones push

---
+++ app/src/main/java/com/example/runworkshop/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.example.runworkshop
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+//Clase para las notificaciones Push
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+}
